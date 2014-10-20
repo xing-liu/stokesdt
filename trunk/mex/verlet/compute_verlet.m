983f41a35b066737829c32af305f6f1e3a1dd6c5
etchow@gmail.com <etchow@gmail.com@598fe8ad-2dd1-bedc-6592-574795029ffd>
2014-10-20 05:02:16 +0000

    document m-file

--- trunk/mex/verlet/compute_verlet.m
+++ trunk/mex/verlet/compute_verlet.m
@@ -3,12 +3,12 @@ function [pi, pj] = compute_verlet(pos, rdi, L, cutoff, mode)
 %   Compute short-range interactions between all the particles.
 %   The particle pairs within the cutoff distance are stored in [pi, pj].
 % INPUT
-%   pos     the array of particle coordinates
-%   rdi     the array of particle radii
+%   pos     the array of particle coordinates (3*npos x 1)
+%   rdi     the array of particle radii (npos x 1)
 %   L       the simulation box size
 %   cutoff  the cutoff
-%   mode    'R' (absulote distance) or 'S' (normalized distance)
+%   mode    'R' (absolute distance) or 'S' (normalized distance)
 %
 % OUTPUT
-%   pi      the vector of particle identities of pi
-%   pj      the vector of particle identities of pj
+%   pi      the vector of particle indices (first of pair)
+%   pj      the vector of particle indices (second of pair)
