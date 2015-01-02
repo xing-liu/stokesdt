f0717390f77d795336c29fe5f073752751b5a149
etchow <etchow@gmail.com>
2015-01-02 03:31:19 +0000

    matlab/test_pairs.m and force_repulsion_parallel.m

--- tests/matlab/test_pairs.m
+++ tests/matlab/test_pairs.m
@@ -13,40 +13,56 @@ pos = rand(3,npos)*L;
 radii = ones(npos,1);
 
 % interaction cutoff
-cutoff = 0.1;
+cutoff = 0.3;
+% cutoff cannot be greater than L/2
 
-% pi is not a good name
-[indi indj] = compute_verlet(pos(:), radii, L, cutoff, 'R');
+[ia ja] = compute_pairlist(pos(:), radii, L, cutoff, 'R');
 
 % change to 1-based indexing
-indi = indi + 1;
-indj = indj + 1;
+ia = ia + 1;
+ja = ja + 1;
 
-% mark the pairs in a lower triangular array
 mark = zeros(npos,npos);
-for i=1:length(indi)
-  mark(indi(i),indj(i)) = 1;
-end
-
-% how are the pairs ordered?  Lower triang?  Upper triang?
-% can they appear more than once?  will i,j and j,i both be output?
-%%% all pairs output...
-
-% check answer by computing distance between all pairs
-% must compute distances using nearest image
-for j=1:npos
-  for i=j+1:npos
-    d = norm(pos(:,indi(i)) - pos(:,indj(i)));
-    m = mark(indi(i),indj(i));
-    if (d <= cutoff && m ~= 1)
-      error('test failure 1');
-    end
-    if (d > cutoff && m ~= 0)
-      d
-      m
-      error('test failure 2');
-    end
+for i=1:npos
+  for j=ia(i):ia(i+1)-1
+    mark(i,ja(j)) = 1;
   end
 end
 
+% note: both i,j and j,i are in pairlist
+% note: diagonal entry is stored
+if (nnz(mark) ~= length(ja))
+  error('duplicate entry in pairlist');
+end
+
+% construct pairlist using brute force
+% must compute distances using nearest image
+brute = zeros(npos,npos);
+for j=1:npos
+  for i=j:npos
+    r = pos(:,i) - pos(:,j);
+
+    % compute minimum image
+    r = mod(r, L);  % assumes mod(-0.1,1)=0.9
+    if r(1) > L/2, r(1) = r(1)-L; end
+    if r(2) > L/2, r(2) = r(2)-L; end
+    if r(3) > L/2, r(3) = r(3)-L; end
+
+    s2 = r(1)*r(1) + r(2)*r(2) + r(3)*r(3);
+
+    if (s2 <= cutoff*cutoff)
+      brute(i,j) = 1;
+    end
+  end
+end
+brute = spones(brute+brute');
+
+% compare
+if (norm(mark-brute,'fro') == 0)
+  fprintf('PASS\n');
+else
+  error('FAIL');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % test polydisperse case
